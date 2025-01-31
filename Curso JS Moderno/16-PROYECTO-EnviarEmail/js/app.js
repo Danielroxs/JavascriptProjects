f90f2376698fa47320f471754719d284e8babb52
Danielroxs <danroxsll@gmail.com>
2025-01-30 20:01:37 -0600

    Tienda autos filter

--- Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -2,34 +2,74 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const email = {
         email: '',
+        cc: '', // Este campo será opcional
         asunto: '',
         mensaje: '',
     }
 
     // Seleccionar los elementos de la interfaz 
     const inputEmail = document.querySelector('#email');
+    const inputCC = document.querySelector('#cc'); // Nuevo campo opcional
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
     const btnSubmit = document.querySelector('#formulario button[type="submit"]')
+    const btnReset = document.querySelector('#formulario button[type="reset"]')
+    const spinner = document.querySelector('#spinner')
 
     // Asignar eventos
-    inputEmail.addEventListener('blur', validar)
-    inputAsunto.addEventListener('blur', validar)
-    inputMensaje.addEventListener('blur', validar)
+    inputEmail.addEventListener('input', validar)
+    inputCC.addEventListener('input', validar) // Agregar validación para cc
+    inputAsunto.addEventListener('input', validar)
+    inputMensaje.addEventListener('input', validar)
+
+    formulario.addEventListener('submit', enviarEmail)
+
+    btnReset.addEventListener('click', function (e) {
+        e.preventDefault();
+        resetFormulario()
+    })
+
+    function enviarEmail(e) {
+        e.preventDefault()
+        spinner.classList.add('flex')
+        spinner.classList.remove('hidden')
+
+        setTimeout(() => {
+            spinner.classList.add('hidden')
+            spinner.classList.remove('flex')
+
+            // Reiniciar el objeto
+            resetFormulario()
+
+            // Crear una alerta
+            const alertaExito = document.createElement('P')
+            alertaExito.classList.add('bg-green-500', 'text-white', 'p-2', 'text-center', 'rounded-lg', 'mt-10', 'font-bold', 'text-sm', 'uppercase')
+            alertaExito.textContent = 'Mensaje enviado correctamente'
+
+            formulario.appendChild(alertaExito)
+
+            setTimeout(() => {
+                alertaExito.remove();
+            }, 3000)
+        }, 3000)
+    }
 
     function validar(e) {
         const id = e.target.id;
-        const texto = e.target.value;
-        const referencia = e.target.parentElement
-        if (e.target.value.trim() === '') {
-            mostrarAlerta(`el campo ${id} es obligatorio`, e.target.parentElement);
+        const texto = e.target.value.trim();
+        const referencia = e.target.parentElement;
+
+        if (texto === '' && id !== 'cc') { // cc es opcional, los demás campos no
+            mostrarAlerta(`El campo ${id} es obligatorio`, referencia);
+            email[e.target.name] = ''
             comprobarEmail()
             return;
         }
 
-        if (id === 'email' && !validarEmail(texto)) {
-            mostrarAlerta('El email no es valido', referencia)
+        if ((id === 'email' || id === 'cc') && texto !== '' && !validarEmail(texto)) {
+            mostrarAlerta('El email no es válido', referencia)
+            email[e.target.name] = ''
             comprobarEmail()
             return
         }
@@ -37,15 +77,13 @@ document.addEventListener('DOMContentLoaded', function () {
         limpiarAlerta(referencia)
 
         // Asignar los valores
-        email[e.target.name] = e.target.value.trim().toLowerCase()
-
+        email[e.target.name] = texto.toLowerCase()
 
         // Comprobar el objeto email
         comprobarEmail()
     }
 
     function mostrarAlerta(mensaje, referencia) {
-
         limpiarAlerta(referencia)
 
         // Generar alerta en HTML
@@ -53,7 +91,7 @@ document.addEventListener('DOMContentLoaded', function () {
         error.textContent = mensaje;
         error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center')
 
-        // Inyectar ERROR al formulario "Pintar en el HTML"
+        // Inyectar ERROR al formulario
         referencia.appendChild(error)
     }
 
@@ -66,17 +104,35 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function validarEmail(email) {
-        const regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/
-        const resultado = regex.test(email)
-        return resultado;
+        const regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+        return regex.test(email);
     }
 
     function comprobarEmail() {
-        if (Object.values(email).includes('')) {
+        console.log(email);
 
-        } else {
-            btnSubmit.classList.remove('opacity-50');
-            btnSubmit.disabled = false;
+        // Excluir `cc` de la validación de obligatoriedad
+        const camposObligatorios = { ...email };
+        delete camposObligatorios.cc; // cc no es obligatorio
+
+        if (Object.values(camposObligatorios).includes('')) {
+            btnSubmit.classList.add('opacity-50');
+            btnSubmit.disabled = true;
+            return;
         }
+
+        btnSubmit.classList.remove('opacity-50');
+        btnSubmit.disabled = false;
+    }
+
+    function resetFormulario() {
+        // Reiniciar el objeto email
+        email.email = ''
+        email.cc = ''
+        email.asunto = ''
+        email.mensaje = ''
+        formulario.reset()
+
+        comprobarEmail()
     }
 })
--- Curso JS Moderno/17-PROYECTO-Buscador/js/db.js
+++ Curso JS Moderno/17-PROYECTO-Buscador/js/db.js
@@ -2,43 +2,43 @@ const autos = [
 	{
 		marca: 'BMW',
 		modelo: 'Serie 3',
-		year: 2020,
+		year: 2024,
 		precio: 30000,
 		puertas: 4,
 		color: 'Blanco',
 		transmision: 'automatico'
 	},
-	{ 
-        marca: 'Audi', 
-        modelo: 'A4', 
-        year: 2020, 
-        precio: 40000, 
-        puertas: 4, 
-        color: 'Negro', 
-        transmision: 'automatico' 
-    },
+	{
+		marca: 'Audi',
+		modelo: 'A4',
+		year: 2024,
+		precio: 40000,
+		puertas: 4,
+		color: 'Negro',
+		transmision: 'automatico'
+	},
 	{
 		marca: 'Ford',
 		modelo: 'Mustang',
-		year: 2015,
+		year: 2021,
 		precio: 20000,
 		puertas: 2,
 		color: 'Blanco',
 		transmision: 'automatico'
 	},
-	{ 
-        marca: 'Audi', 
-        modelo: 'A6', 
-        year: 2020, 
-        precio: 35000, 
-        puertas: 4, 
-        color: 'Negro', 
-        transmision: 'automatico' 
-    },
+	{
+		marca: 'Audi',
+		modelo: 'A6',
+		year: 2024,
+		precio: 35000,
+		puertas: 4,
+		color: 'Negro',
+		transmision: 'automatico'
+	},
 	{
 		marca: 'BMW',
 		modelo: 'Serie 5',
-		year: 2016,
+		year: 2022,
 		precio: 70000,
 		puertas: 4,
 		color: 'Rojo',
@@ -47,7 +47,7 @@ const autos = [
 	{
 		marca: 'Mercedes Benz',
 		modelo: 'Clase C',
-		year: 2015,
+		year: 2021,
 		precio: 25000,
 		puertas: 4,
 		color: 'Blanco',
@@ -56,44 +56,43 @@ const autos = [
 	{
 		marca: 'Chevrolet',
 		modelo: 'Camaro',
-		year: 2018,
+		year: 2023,
 		precio: 60000,
 		puertas: 2,
 		color: 'Rojo',
 		transmision: 'manual'
 	},
-	{ 
-        marca: 'Ford', 
-        modelo: 'Mustang', 
-        year: 2019, 
-        precio: 80000, 
-        puertas: 2, 
-        color: 'Rojo', 
-        transmision: 'manual' 
-    },
+	{
+		marca: 'Ford',
+		modelo: 'Mustang',
+		year: 2023,
+		precio: 80000,
+		puertas: 2,
+		color: 'Rojo',
+		transmision: 'manual'
+	},
 	{
 		marca: 'Dodge',
 		modelo: 'Challenger',
-		year: 2020,
+		year: 2024,
 		precio: 40000,
 		puertas: 4,
 		color: 'Blanco',
 		transmision: 'automatico'
 	},
-	{ 
-        marca: 'Audi', 
-        modelo: 'A3', 
-        year: 2017, 
-        precio: 55000, 
-        puertas: 2, 
-        color: 'Negro', 
-        transmision: 
-        'manual' 
-    },
+	{
+		marca: 'Audi',
+		modelo: 'A3',
+		year: 2022,
+		precio: 55000,
+		puertas: 2,
+		color: 'Negro',
+		transmision: 'manual'
+	},
 	{
 		marca: 'Dodge',
 		modelo: 'Challenger',
-		year: 2020,
+		year: 2024,
 		precio: 25000,
 		puertas: 2,
 		color: 'Rojo',
@@ -102,7 +101,7 @@ const autos = [
 	{
 		marca: 'Mercedes Benz',
 		modelo: 'Clase C',
-		year: 2018,
+		year: 2023,
 		precio: 45000,
 		puertas: 4,
 		color: 'Azul',
@@ -111,25 +110,25 @@ const autos = [
 	{
 		marca: 'BMW',
 		modelo: 'Serie 5',
-		year: 2019,
+		year: 2023,
 		precio: 90000,
 		puertas: 4,
 		color: 'Blanco',
 		transmision: 'automatico'
 	},
-	{ 
-        marca: 'Ford', 
-        modelo: 'Mustang', 
-        year: 2017, 
-        precio: 60000, 
-        puertas: 2, 
-        color: 'Negro', 
-        transmision: 'manual' 
-    },
+	{
+		marca: 'Ford',
+		modelo: 'Mustang',
+		year: 2022,
+		precio: 60000,
+		puertas: 2,
+		color: 'Negro',
+		transmision: 'manual'
+	},
 	{
 		marca: 'Dodge',
 		modelo: 'Challenger',
-		year: 2015,
+		year: 2021,
 		precio: 35000,
 		puertas: 2,
 		color: 'Azul',
@@ -138,7 +137,7 @@ const autos = [
 	{
 		marca: 'BMW',
 		modelo: 'Serie 3',
-		year: 2018,
+		year: 2023,
 		precio: 50000,
 		puertas: 4,
 		color: 'Blanco',
@@ -147,7 +146,7 @@ const autos = [
 	{
 		marca: 'BMW',
 		modelo: 'Serie 5',
-		year: 2017,
+		year: 2022,
 		precio: 80000,
 		puertas: 4,
 		color: 'Negro',
@@ -156,19 +155,19 @@ const autos = [
 	{
 		marca: 'Mercedes Benz',
 		modelo: 'Clase C',
-		year: 2018,
+		year: 2023,
 		precio: 40000,
 		puertas: 4,
 		color: 'Blanco',
 		transmision: 'automatico'
 	},
-	{ 
-        marca: 'Audi', 
-        modelo: 'A4', 
-        year: 2016, 
-        precio: 30000, 
-        puertas: 4, 
-        color: 'Azul', 
-        transmision: 'automatico' 
-    }
+	{
+		marca: 'Audi',
+		modelo: 'A4',
+		year: 2022,
+		precio: 30000,
+		puertas: 4,
+		color: 'Azul',
+		transmision: 'automatico'
+	}
 ];
