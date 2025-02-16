868f187f0432ef53bd29e4ec0dc0b2bb30c152f7
Danielroxs <danroxsll@gmail.com>
2025-02-16 13:11:37 -0600

    Clinica animal

--- Curso JS Moderno/26-PROYECTO-AdministrarCitas/js/app.js
+++ Curso JS Moderno/26-PROYECTO-AdministrarCitas/js/app.js
@@ -31,12 +31,49 @@ const citaObj = {
 
 // Funciones
 function datosCita(e) {
-    citaObj[e.target.name] = e.target.value;
-    console.log(citaObj)
+    citaObj[e.target.name] = e.target.value.trim()
 }
 
 function submitCita(e) {
     e.preventDefault()
 
-    console.log('Submit al formulario...')
+    if (Object.values(citaObj).some(valor => valor.trim() === '')) { // si en los valores osea el value de las propiedades del objeto "citaObj" incluyen ""...
+        new Notificacion({ texto: 'Todos los campos son obligatorios', tipo: 'error' })
+        return
+    }
+}
+
+class Notificacion {
+    constructor({ texto, tipo }) {
+        this.texto = texto;
+        this.tipo = tipo;
+
+        this.mostrar()
+    }
+
+    mostrar() {
+        // Crear la notificacion
+        const alerta = document.createElement('DIV')
+        alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 'my-2', 'alert', 'uppercase', 'font-bold', 'text-sm')
+
+        // Eliminar alertas duplicadas
+        const alertaprevia = document.querySelector('.alert')
+        alertaprevia?.remove()
+
+
+        // Si es de tipo error, agrega una clase
+        this.tipo === 'error' ? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500')
+
+        // Mensaje de error
+        alerta.textContent = this.texto
+
+        // Insertar en el DOM 
+        formulario.parentElement.insertBefore(alerta, formulario)
+
+        // Quitar despues de 5 segundos
+        setTimeout(() => {
+            alerta.remove()
+        }, 3000);
+    }
+
 }
