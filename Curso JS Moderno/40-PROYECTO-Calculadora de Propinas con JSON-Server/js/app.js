0a5fb90d6cd495dfaf0d73ec316007b138f7be5e
Danielroxs <danroxsll@gmail.com>
2025-03-16 15:02:42 -0600

    API REST connection

---
+++ Curso JS Moderno/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -0,0 +1,101 @@
+let cliente = {
+    mesa: '',
+    hora: '',
+    pedido: []
+};
+
+const categorias = {
+    1: "Comida",
+    2: "Bebidas",
+    3: "Postres"
+}
+
+const btnGuardarCliente = document.querySelector('#guardar-cliente')
+btnGuardarCliente.addEventListener('click', guardarCliente)
+
+function guardarCliente() {
+    const mesa = document.querySelector('#mesa').value
+    const hora = document.querySelector('#hora').value
+
+    // Revisar si hay campos vacios
+    const camposVacios = [mesa, hora].some(campo => campo === "")
+
+    if (camposVacios) {
+        const existeError = document.querySelector('.invalid-feedback')
+
+        // Verifica si existe error, sino lo crea
+        if (!existeError) {
+            const alerta = document.createElement('DIV')
+            alerta.classList.add('invalid-feedback', 'd-block', 'text-center')
+            alerta.textContent = "Todos los campos son obligatorios"
+            document.querySelector('.modal-body form').appendChild(alerta)
+
+            // Eliminar la alerta con un retraso de 3s
+            setTimeout(() => {
+                alerta.remove()
+            }, 3000);
+        }
+
+        return
+
+    }
+
+    // Asignar datos del formulario a cliente
+    cliente = { ...cliente, mesa, hora }
+
+    // Ocultar modal
+    const modalFormulario = document.querySelector('#formulario')
+    const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario)
+    if (modalBootstrap) {
+        modalBootstrap.hide()
+    }
+
+    // Mostrar las secciones
+    mostrarSecciones()
+
+    // Obtener platillos de la API  de JSON-Server
+    obtenerPlatillos()
+}
+
+function mostrarSecciones() {
+    const seccionesOcultas = document.querySelectorAll('.d-none');
+    seccionesOcultas.forEach(seccion => seccion.classList.remove('d-none'));
+
+}
+
+function obtenerPlatillos() {
+    const url = 'http://localhost:4000/platillos'
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(respuesta => mostrarPlatillos(respuesta))
+        .catch(error => console.log(error))
+}
+
+function mostrarPlatillos(platillos) {
+    const contenido = document.querySelector('#platillos .contenido')
+
+    platillos.forEach(platillo => {
+        const row = document.createElement('DIV')
+        row.classList.add('row', 'py-3', 'border-top')
+
+        const nombre = document.createElement('DIV')
+        nombre.classList.add('col-md-4')
+        nombre.textContent = platillo.nombre
+
+        const precio = document.createElement('DIV')
+        precio.classList.add('col-md-3', 'fw-bold')
+        precio.textContent = `$${platillo.precio}`
+
+        const categoria = document.createElement('DIV')
+        categoria.classList.add('col-md-3')
+        categoria.textContent = categorias[platillo.categoria]
+
+        row.appendChild(nombre)
+        row.appendChild(precio)
+        row.appendChild(categoria)
+
+        contenido.appendChild(row)
+    })
+
+}
