6df27c71747d95ede89703dcca8ee6ea6df7639d
Danielroxs <danroxsll@gmail.com>
2025-04-07 12:49:59 -0600

    Header and menu navigation

--- Terminados/agenciaViajesNode/routes/index.js
+++ Terminados/agenciaViajesNode/routes/index.js
@@ -3,9 +3,12 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/', (req, res) => { // req - lo que enviamos   res - lo que express nos responde, peticion y respuesta
-    res.send('Inicio')
+    res.render('inicio')
 })
 router.get('/nosotros', (req, res) => { // req - lo que enviamos   res - lo que express nos responde, peticion y respuesta
+
+    const viajes = 'Cambiando el texto otra vez'
+
     res.render('nosotros')
 })
 
