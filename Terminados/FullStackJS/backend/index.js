55da7d7e5c8be3894817aec26a664aece50f8978
Danielroxs <danroxsll@gmail.com>
2025-04-15 15:50:19 -0600

    BD & Server NodeJs

---
+++ Terminados/FullStackJS/backend/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import conectarDB from './config/db.js'
+
+const app = express()
+
+conectarDB()
+
+app.use("/", (req, res) => {
+    res.send("Hola mundo")
+})
+
+app.listen(4000, () => {
+    console.log('Servidor funcionando el el puerto 4000')
+})
