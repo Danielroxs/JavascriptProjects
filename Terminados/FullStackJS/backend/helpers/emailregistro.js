0106521ef37a4eea9ba26e7dc5b7de36ee89b789
Danielroxs <danroxsll@gmail.com>
2025-04-27 21:14:08 -0600

    bug fixed

--- Terminados/FullStackJS/backend/helpers/emailregistro.js
+++ Terminados/FullStackJS/backend/helpers/emailregistro.js
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer'
 
-const emailregistro = (datos) => {
+const emailregistro = async (datos) => {
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
@@ -10,10 +10,22 @@ const emailregistro = (datos) => {
         }
     });
 
-    console.log(datos)
+    const { email, nombre, token } = datos
 
     // Enviar el email
+    const info = await transporter.sendMail({
+        from: "APV - Administrador de Pacientes de Veterinaria",
+        to: email,
+        subject: "Comprueba tu Cuenta en APV",
+        html: `
+            <p>Hola ${nombre},</p>
+            <p>Gracias por registrarte en APV. Para confirmar tu cuenta, haz clic en el siguiente enlace:</p>
+            <a href="${process.env.FRONTEND_URL}/confirmar/${token}" target="_blank">Confirmar Cuenta</a>
+            <p>Si no creaste esta cuenta, puedes ignorar este mensaje.</p>
+        `
+    });
 
+    console.log("Mensaje enviado: %", info.messageId)
 }
 
 export default emailregistro;
--- Terminados/FullStackJS/backend/routes/veterinarioRoutes.js
+++ Terminados/FullStackJS/backend/routes/veterinarioRoutes.js
@@ -1,27 +1,24 @@
-// Importamos el módulo de express
 import express from 'express';
-// Creamos una instancia del router de express
 const router = express.Router();
-// Importamos las funciones del controlador de veterinario
-import { registrar, perfil, confirmar, autenticar, olvidePassword, comprobarToken, nuevoPassword } from '../controllers/veterinarioController.js';
-// Importamos el middleware de autenticación
+import {
+    registrar,
+    perfil,
+    confirmar,
+    autenticar,
+    olvidePassword,
+    comprobarToken,
+    nuevoPassword
+} from '../controllers/veterinarioController.js';
 import checkAuth from '../middleware/authMiddleware.js';
 
-// Rutas para el área pública
-// Ruta para registrar un nuevo veterinario
+// Area publica
 router.post('/', registrar);
-// Ruta para confirmar la cuenta de un veterinario mediante un token
 router.get('/confirmar/:token', confirmar);
-// Ruta para autenticar (iniciar sesión) un veterinario
 router.post('/login', autenticar);
-// Ruta para solicitar el restablecimiento de contraseña
 router.post('/olvide-password', olvidePassword);
-// Ruta para comprobar el token y establecer una nueva contraseña
 router.route('/olvide-password/:token').get(comprobarToken).post(nuevoPassword);
 
-// Rutas para el área privada
-// Ruta para obtener el perfil del veterinario autenticado
+// Area privada
 router.get('/perfil', checkAuth, perfil);
 
-// Exportamos el router para que pueda ser utilizado en otras partes de la aplicación
 export default router;
