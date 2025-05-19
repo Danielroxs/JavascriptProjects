b07ea6c34550b705c1c82a16aedc84696b58ba06
Danielroxs <danroxsll@gmail.com>
2025-05-19 16:25:01 -0600

    APIKEY OPENROUTER

---
+++ Terminados/ChatIA/nucleus-js-ia/src/main.js
@@ -0,0 +1,17 @@
+import './style.css'
+
+const form = document.querySelector('#form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const prompt = document.querySelector('#prompt').value
+
+    if (prompt.trim() === '') {
+        alert('La consulta no puede ir vacia')
+        return
+    }
+
+    console.log(prompt)
+
+})
