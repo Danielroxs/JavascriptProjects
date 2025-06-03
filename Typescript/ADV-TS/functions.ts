ec20a51af757a976f1b51fe10d7573ffc68836be
Danielroxs <danroxsll@gmail.com>
2025-06-02 22:41:09 -0600

    Sobrecarga de funciones

---
+++ Typescript/ADV-TS/functions.ts
@@ -0,0 +1,14 @@
+// ---------------FUNCIONES SOBRECARGADAS
+function getLength(val: any[]): number
+function getLength(val: string): string
+function getLength(val: string | any[]){
+    if(typeof val === 'string'){
+        const numberOfWords = val.split(' ').length;
+        return `${numberOfWords} words`
+    }
+    return val.length
+}
+
+const numberOfWords = getLength('does this work?');
+numberOfWords.length
+const numItems = getLength(['Sports', 'Cookies']) 
