539b4afd22a19e15fe5cafbeb22e9d2de2a1be60
Danielroxs <danroxsll@gmail.com>
2025-06-04 16:02:18 -0600

    Derivacion de tipos a partir de otros tipos

---
+++ Typescript/TYPES-FROM-TYPES/typeof.js
@@ -0,0 +1,3 @@
+"use strict";
+const userName = 'Dan';
+console.log(typeof userName);
---
+++ Typescript/TYPES-FROM-TYPES/typeof.ts
@@ -0,0 +1,18 @@
+const userName = 'Dan'
+
+console.log(typeof userName)
+
+type UserName = typeof userName
+
+
+//------- tipado personalizado automatico con typeof
+const settings = {
+    difficulty: 'easy',
+    minLevel: 10,
+    didStart: false,
+    players: ['Jhon', 'Jane']
+}
+
+// type Settings = typeof settings o bien abajo directamente en la funcion
+
+function loadedData(setting: typeof settings){}
