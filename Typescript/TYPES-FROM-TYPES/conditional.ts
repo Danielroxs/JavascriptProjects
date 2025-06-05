8891c7fd96d6031fab75dbe2993d5d44f0e6f862
Danielroxs <danroxsll@gmail.com>
2025-06-05 17:01:16 -0600

    conditional part 2

--- Typescript/TYPES-FROM-TYPES/conditional.ts
+++ Typescript/TYPES-FROM-TYPES/conditional.ts
@@ -9,3 +9,21 @@ type Example1 = GetElementType<StringArray>
 type Example2 = GetElementType<typeof text>
 let value: Example2;
 value = false
+
+type FullNamePerson = {firstName: string, lastName: string}
+type FullNameOrNothing<T> = T extends FullNamePerson ? string : never
+
+function getFullName<T extends object>(person: T ): FullNameOrNothing<T> {
+    if ('firstName' in person &&
+        'lastName' in person && 
+        person.firstName && 
+        person.lastName
+    ) {
+        return `${person.firstName} ${person.lastName}` as FullNameOrNothing<T>
+    }
+    throw new Error('No first name and/or last name found')
+}
+
+const name1 = getFullName({})
+const name2 = getFullName({firstName: 'Rox'})
+const name3 = getFullName({firstName: 'Rox', lastName: 'Dan'})
