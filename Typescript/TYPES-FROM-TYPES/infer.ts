de78a85aa61d02cbc53734b02b860c5e93294019
Danielroxs <danroxsll@gmail.com>
2025-06-09 12:21:54 -0600

    infer ts

---
+++ Typescript/TYPES-FROM-TYPES/infer.ts
@@ -0,0 +1,10 @@
+// ---------Tipo de Valor de Retorno
+
+function add(a: number, b: number){
+    return a + b;
+}
+
+type AddFn = typeof add;
+type ReturnValueType<T> = T extends (...args: any[]) => infer RV ? RV : never;
+
+type AddFnReturnValueType = ReturnType<AddFn>
