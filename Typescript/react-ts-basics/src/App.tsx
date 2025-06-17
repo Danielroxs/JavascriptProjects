9b036bc8e9289b809781e8507e629b671134e70e
Danielroxs <danroxsll@gmail.com>
2025-06-17 14:47:41 -0600

    Type for React Component

---
+++ Typescript/react-ts-basics/src/App.tsx
@@ -0,0 +1,14 @@
+import CourseGoal from "./components/CourseGoal.tsx";
+
+const App = () => {
+  return (
+    <main>
+      <CourseGoal
+        title="React + TypeScript"
+        description="Curso de Ts y React"
+      />
+    </main>
+  );
+};
+
+export default App;
