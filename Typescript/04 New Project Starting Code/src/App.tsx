2259bd76060f356c2c8bc5545192c1170c9062d9
Danielroxs <danroxsll@gmail.com>
2025-06-23 12:53:31 -0600

    useRef en TS

--- Typescript/04 New Project Starting Code/src/App.tsx
+++ Typescript/04 New Project Starting Code/src/App.tsx
@@ -1,19 +1,13 @@
 //import Input from "./components/Input";
-import Button from "./components/Button";
-import Container from "./components/Container";
+import { useRef } from "react";
+import Input from "./components/Input";
 
 function App() {
+  const input = useRef<HTMLInputElement>(null);
+
   return (
     <main>
-      <Container
-        as={Button}
-        onClick={() => {
-          console.log("hola");
-        }}
-        type="button"
-      >
-        Click me
-      </Container>
+      <Input label="Test" id="test" ref={input} />
     </main>
   );
 }
