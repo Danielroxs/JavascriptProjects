82a1011eca12c6d119c7b1ccdcbdccc44e096b60
Danielroxs <danroxsll@gmail.com>
2025-06-27 21:11:42 -0600

    TryCatch & Spinner

--- Typescript/04 New Project Starting Project/src/App.tsx
+++ Typescript/04 New Project Starting Project/src/App.tsx
@@ -1,7 +1,72 @@
 import { get } from "./util/http";
+import BlogPosts, { type BlogPost } from "./components/BlogPosts.tsx";
+import { type ReactNode, useEffect, useState } from "react";
+import fetchingImg from "./assets/data-fetching.png";
+import ErrorMessage from "./components/ErrorMessage.tsx";
+
+type RawDataBlogPost = {
+  id: number;
+  userId: string;
+  title: string;
+  body: string;
+};
 
 function App() {
-  get("https://jsonplaceholder.typicode.com/posts");
+  const [fetchedPost, setFetchedPost] = useState<BlogPost[]>();
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState<string>();
+
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true);
+
+      try {
+        const data = (await get(
+          "https://jsonplaceholder.typicode.com/posts"
+        )) as RawDataBlogPost[];
+        const blogPosts: BlogPost[] = data.map((rawPost) => {
+          return {
+            id: rawPost.id,
+            title: rawPost.title,
+            text: rawPost.body,
+          };
+        });
+        setFetchedPost(blogPosts);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
+        }
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchPosts();
+  }, []);
+
+  let content: ReactNode;
+
+  if (error) {
+    content = <ErrorMessage text={error} />;
+  }
+
+  if (fetchedPost) {
+    content = <BlogPosts posts={fetchedPost} />;
+  }
+
+  if (isFetching) {
+    content = <p id="loading-fallback">Fetching posts...</p>;
+  }
+
+  return (
+    <main>
+      <img
+        src={fetchingImg}
+        alt="An abstract image depicting a data fetching process."
+      />
+      {content}
+    </main>
+  );
 }
 
 export default App;
